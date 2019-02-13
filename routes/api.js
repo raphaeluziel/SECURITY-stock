143c454e4d6dd9d51e342d6ab1f5855ad917c500
Glitch (security-stock) <none>
2019-02-13 01:21:46 +0000

    👩‍👩‍👦⛸ Updated with Glitch

--- routes/api.js
+++ routes/api.js
@@ -43,9 +43,8 @@ module.exports = function (app) {
   
   app.route('/api/stock-prices')
     .get(function (req, res){
-    
-    //console.log(req.query);
-    
+    console.log(req.query);
+
       app.set('trust proxy',true);
       
       var stock = [];
@@ -59,34 +58,10 @@ module.exports = function (app) {
       else{
         stock[0] = req.query.stock.toUpperCase();
       }
-
-      if (req.query.like == 'true') { like = 1; } else { like = 0; }
-      
-      stockModel.findOne({stock: stock}, function(err, data){
-        if(err) { return console.log('error accessing database'); }
-        if(data) {
-          if (req.query.like == 'true') { like = 1; } else { like = 0; }
-          if (data.ip.indexOf(ipAddress) === -1){
-            data.ip.push(ipAddress);
-            data.likes += like;     
-          }
-          data.save(function(err, data){
-            likeCount = data.likes;
-          });
-        }
-        if(!data) {
-          var newStock = new stockModel({
-            stock: stock,
-            likes: like,
-            ip: ipAddress
-          })
-          newStock.save(function(err, data){
-            likeCount = data.likes;
-          });  
-        }
-
-      });
-
+    
+    
+    
+    
       var link = ['https://api.iextrading.com/1.0/stock/' + stock[0] + '/price', 
                   'https://api.iextrading.com/1.0/stock/' + stock[1] + '/price']
     
@@ -110,6 +85,55 @@ module.exports = function (app) {
             
           }).catch(function (err) { res.send('stock does not exist'); });
         }).catch(function (err) { res.send('stock does not exist'); });
+    
+    
+    
+    
+    
+    
+
+      if (req.query.like == 'true') { like = 1; } else { like = 0; }
+    
+    
+
+    console.log(stock);  
+    stock.forEach(function(x){
+      stockModel.findOne({stock: x}, function(err, data){
+        console.log(data);
+        console.log(stock);
+        if(err) { return console.log('error accessing database'); }
+        if(data) {
+          if (req.query.like == 'true') { like = 1; } else { like = 0; }
+          if (data.ip.indexOf(ipAddress) === -1){
+            data.ip.push(ipAddress);
+            data.likes += like;     
+          }
+          data.save(function(err, data){
+            console.log("Updated stock");
+            likeCount = data.likes;
+          });
+        }
+        if(!data) {
+          var newStock = new stockModel({
+            stock: x,
+            likes: like,
+            ip: ipAddress
+          })
+          newStock.save(function(err, data){
+            console.log("saved new stock");
+            likeCount = like;
+          });  
+        }
+
+      });
+       
+
+    })
+    
+    
+    
+    
+    
     
     });
     
